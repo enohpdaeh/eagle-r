1cfa9475dbfeb31c9bbcfc55d00c09e5384a278c
enohpdaeh <enohpdaeh@gmail.com>
2015-03-21 04:39:57 +0900

    サイトリニューアルに対応

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var request = require('request');
 var client = require('cheerio-httpcli');
-client.headers['User-Agent'] = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; ja-jp) AppleWebKit/533.17.9 (KHTML,like Gecko) Version/5.0.2 Mobile/8C148a Safari/6533.18.5';
+//client.headers['User-Agent'] = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_2_1 like Mac OS X; ja-jp) AppleWebKit/533.17.9 (KHTML,like Gecko) Version/5.0.2 Mobile/8C148a Safari/6533.18.5';
+client.setBrowser('chrome');
 client.timeout = 240000;
 
 now = new Date();
@@ -16,16 +17,34 @@ pd = ("0" + pre.getDate()).slice(-2);
 var yesterday = py + '-' + pm + '-' + pd;
 var num = [];
 var i = 0;
+var start = "";
 
 console.log(yesterday);
-console.log('http://dd.eagle-r-s.net/hl/101/machine-information/machine-detail?rack_no=' + i + '&target_date=' + yesterday);
+console.log('http://eagle.pt.teramoba2.com/rshounandai/standgraph/?rack_no=' + i + '&target_date=' + yesterday);
+
 for(i = 638; i <= 1085; i++){
   if(i.toString().match(/4|9|688|70.|71.|72.|73[012]|78[35678]|80.|810|83[1235678]|8[5678].|10[01].|102[01]/)){
   }else{
     num.push(i.toString());
-    client.fetch('http://dd.eagle-r-s.net/hl/101/machine-information/machine-detail?rack_no=' + i + '&target_date=' + yesterday, {}, function (err, $, res) {
-        console.log($('h2#machine_name').text().replace(/\s+/g, "") + $('div.veiw_rack_no').text().replace(/\s+/g, ""));
+    client.fetch('http://eagle.pt.teramoba2.com/rshounandai/standgraph/?rack_no=' + i + '&target_date=' + yesterday, {}, function (err, $, res) {
+        console.log($('.box-base .box-article01 .st01').text().replace(/[\s+当り履歴スランプグラフ過去データ週間検索]/g, "").slice(0,15) + $('.box-base .box-article01 .navigate .current').text().replace(/\s+/g, ""));
+        $('.box-base .box-article01 .banBox .tables .left ol li img').each(function (idx) {
+          start = start + $(this).attr('alt');
+        });
+        console.log(start.slice(4,8));
+        start = "";
+        });
+  }
+}
+/*
+for(i = 638; i <= 1085; i++){
+  if(i.toString().match(/4|9|688|70.|71.|72.|73[012]|78[35678]|80.|810|83[1235678]|8[5678].|10[01].|102[01]/)){
+  }else{
+    num.push(i.toString());
+    client.fetch('http://eagle.pt.teramoba2.com/rshounandai/standgraph/?rack_no=' + i + '&target_date=' + yesterday, {}, function (err, $, res) {
+        console.log($('h2#st01 title').text().replace(/\s+/g, "") + $('div.navigate').text().replace(/\s+/g, ""));
         console.log($('div.detail_number').text().replace(/\s+/g, ""));
     });
   }
 }
+*/
